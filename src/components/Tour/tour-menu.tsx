61733ee833de586e4612bfe964ad209d2e0b30e1
arthurpph <arthurpiana13@gmail.com>
2025-08-04 23:14:58 -0300

    fix: fixed icon not centralized

--- src/components/Tour/tour-menu.tsx
+++ src/components/Tour/tour-menu.tsx
@@ -63,9 +63,9 @@ export default function TourMenu() {
             </nav>
 
             {isPending && (
-               <div className="absolute top-full mt-2 flex w-full justify-center text-cyan-300">
+               <div className="mt-4 flex justify-center text-cyan-300">
                   <svg
-                     className="h-5 w-5 animate-spin sm:h-6 sm:w-6"
+                     className="h-6 w-6 animate-spin text-cyan-300"
                      xmlns="http://www.w3.org/2000/svg"
                      fill="none"
                      viewBox="0 0 24 24"
@@ -77,12 +77,12 @@ export default function TourMenu() {
                         r="10"
                         stroke="currentColor"
                         strokeWidth="4"
-                     ></circle>
+                     />
                      <path
                         className="opacity-75"
                         fill="currentColor"
                         d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-                     ></path>
+                     />
                   </svg>
                </div>
             )}
