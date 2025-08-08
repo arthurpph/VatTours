5f474e934ace13b79937314c0f695f5373b82806
arthurpph <arthurpiana13@gmail.com>
2025-08-08 17:25:50 -0300

    style: Apply Prettier formatting to config and CSS files
    - Format jest.config.js with proper indentation
    - Add missing newline to jest.setup.js
    - Format CSS properties alignment in globals.css

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,22 @@
 module.exports = {
-  testEnvironment: 'jsdom',
-  testMatch: ['**/__tests__/**/*.(test|spec).(ts|tsx)', '**/*.(test|spec).(ts|tsx)'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', {
-      tsconfig: {
-        jsx: 'react-jsx',
-      },
-    }],
-  },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+   testEnvironment: 'jsdom',
+   testMatch: [
+      '**/__tests__/**/*.(test|spec).(ts|tsx)',
+      '**/*.(test|spec).(ts|tsx)',
+   ],
+   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+   transform: {
+      '^.+\\.(ts|tsx)$': [
+         'ts-jest',
+         {
+            tsconfig: {
+               jsx: 'react-jsx',
+            },
+         },
+      ],
+   },
+   moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/src/$1',
+   },
+   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
 };
